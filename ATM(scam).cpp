f521e00a055ba48b2386f6a783b34791ac7e5bae
Om Parulekar <65206237+Sakura1805@users.noreply.github.com>
2021-07-25 07:29:52 +0530

    Add files via upload

---
+++ ATM(scam).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	double y;
+	int x;
+	
+	cin>>x>>y;
+	
+	double ans=y;
+	
+	if(x%5==0 && y-x-0.5>=0)
+	ans=ans-x-0.5;
+	
+	cout<<ans<<endl;
+	
+	return 0;
+}
