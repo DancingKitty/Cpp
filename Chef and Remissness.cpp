4b069c4e3f71bfeee275c4d5fb5e43921d165623
Om Parulekar <65206237+Sakura1805@users.noreply.github.com>
2021-07-25 16:53:08 +0530

    Add files via upload

---
+++ Chef and Remissness.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	
+	for(int i=0; i<n; i++)
+	{
+		int a,b;
+		cin>>a>>b;
+		
+		if(a>b)
+		{
+			cout<<a<<" "<<a+b<<"\n";
+		}
+		
+		else if(b>a)
+		{
+			cout<<b<<" "<<a+b<<"\n";
+		}
+	}
+}
