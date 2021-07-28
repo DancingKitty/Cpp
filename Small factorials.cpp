677c59ff5394a638b9c2a39e4cefd5ee7204ce96
Om Parulekar <65206237+DancingKitty@users.noreply.github.com>
2021-07-28 08:26:37 +0530

    Add files via upload

---
+++ Small factorials.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,t;
+	cin>>t;
+	
+	for(int i=0; i<t; i++)
+	{
+	cin>>n;
+	vector<int> v;
+	int b=n;
+	while(n--)
+	{
+		v.push_back(n);
+		if(n==0)
+		{
+			break;
+		}
+	}
+	int a=v.size();
+	for(int i=0; i<(a-1); i++)
+	{
+		b=b*v[i];
+	}
+	cout<<b;
+    }
+}
