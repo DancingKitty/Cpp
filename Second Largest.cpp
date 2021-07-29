02b7e94f7c38495aae8178201eaa665b3d5b3cf1
Om Parulekar <65206237+DancingKitty@users.noreply.github.com>
2021-07-29 07:12:02 +0530

    Add files via upload

---
+++ Second Largest.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	
+	for(int i=0; i<n; i++)
+	{
+	
+	int a[3];
+	
+	for(int i=0; i<3; i++)
+	{
+		cin>>a[i];
+	}
+	
+	sort(a,a+3);
+	
+	cout<<a[1]<<"\n";
+	
+    }
+}
