66412894806f77d38e3861ddb8eb5a7d72f2963b
Om Parulekar <65206237+DancingKitty@users.noreply.github.com>
2021-07-31 17:03:31 +0530

    Add files via upload

---
+++ Small Factorial.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	for(int i=0; i<t; i++)
+	{
+		long n;
+		cin>>n;
+		long a=n;
+		while(n--)
+		if (n<0)
+		return 1;
+		
+		else 
+		{
+			n=n-1;
+			a=a*n;
+		}
+		cout<<a<<"\n";
+    }
+}
+
