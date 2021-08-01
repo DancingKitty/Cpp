be82fe3aa40ec6ee966c18b85273f15b54418b71
Om Parulekar <65206237+DancingKitty@users.noreply.github.com>
2021-08-01 08:52:09 +0530

    Add files via upload

---
+++ Is Both Or Not.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	
+	if((n%5)==0 && (n%11)==0)
+	{
+		cout<<"BOTH\n";
+	}
+	
+	else if((n%5)!=0 && (n%11)!=0)
+	{
+		cout<<"NONE\n";
+	}
+	
+	else if((n%5)!=0 && (n%11)==0)
+	{
+		cout<<"ONE\n";
+	}
+	
+	else if((n%5)==0 && (n%11)!=0)
+	{
+		cout<<"ONE\n";
+	}
+}
