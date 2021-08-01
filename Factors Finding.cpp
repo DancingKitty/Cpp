10057cae538b2aac893666e85df445322e1649e2
Om Parulekar <65206237+DancingKitty@users.noreply.github.com>
2021-08-01 08:52:33 +0530

    Add files via upload

---
+++ Factors Finding.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	
+	int a = n;
+	int count=0;
+	vector<int> v;
+	
+	for(int i=1; i<=n; i++)
+	{
+		if((a%i)==0)
+		{
+			v.push_back(i);
+			count++;
+		}
+	}
+	
+	cout<<count<<"\n";
+	
+	int b = v.size();
+	
+	for(int i=0; i<b; i++)
+	{
+		cout<<v[i]<<" ";
+	}
+}
