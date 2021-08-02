4bf3bce336986f970b23fc76dcf68428370806e2
Om Parulekar <65206237+Sakura1805@users.noreply.github.com>
2021-08-03 00:22:24 +0530

    Add files via upload

---
+++ Find Me.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,k,count=0;
+	cin>>n>>k;
+	
+	int a[n];
+	
+	for(int i=0; i<n; i++)
+	{
+		cin>>a[i];
+	}
+	
+	for(int i=0; i<n; i++)
+	{
+		if(a[i]==k)
+		{
+			count++;
+		}
+	}	
+	
+	if(count>0)
+	{
+		cout<<"1";
+	}
+	
+	else if(count==0)
+	{
+		cout<<"-1";
+	}
+}
