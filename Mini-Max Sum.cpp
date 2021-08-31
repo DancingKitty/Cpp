40fb5f2873b64aa125c3a36265bd01d59b00f932
Om Parulekar <65206237+Sakura1805@users.noreply.github.com>
2021-08-31 10:23:13 +0530

    Add files via upload

--- Mini-Max Sum.cpp
+++ Mini-Max Sum.cpp
@@ -2,23 +2,9 @@
 using namespace std;
 int main()
 {
-    long a[5];
-    long l,m,n,o,p;
-    
-    for(int i=0; i<5; i++)
-    {
-        cin>>a[i];
-    }
-    
-    l=a[0]+a[1]+a[2]+a[3];
-    m=a[0]+a[1]+a[2]+a[4];
-    n=a[1]+a[2]+a[3]+a[4];
-    o=a[0]+a[1]+a[3]+a[4];
-    p=a[0]+a[2]+a[3]+a[4];
-    
-    long b[5]={l,m,n,o,p};
-    
-    sort(b,b+5);
-    
-    cout<<b[0]<<" "<<b[4];
+	long a[5];
+	for(long i=0; i<5; i++)
+	cin>>a[i];
+	sort(a,a+5);
+	cout<<(a[0]+a[1]+a[2]+a[3])<<" "<<(a[1]+a[2]+a[3]+a[4]);
 }
