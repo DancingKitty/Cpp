27734a756c7dafa4d50b35d417c682f403105a05
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2021-09-23 00:04:31 +0530

    Add files via upload

--- Arrays Introduction.cpp
+++ Arrays Introduction.cpp
@@ -2,18 +2,12 @@
 using namespace std;
 int main()
 {
-    int n;
-    int a[1000];
-    cin>>n;
-    
-    for(int i=0;i<n;i++)
-    {
-        cin>>a[i];
-    }
-    
-    for(int j=(n-1); j>=0; j--)
-    {
-        cout<<a[j]<<" ";
-    }
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0; i<n; i++)
+	cin>>a[i];
+	for(int i=(n-1); i>=0; i--)
+	cout<<a[i]<<" ";
 }
 
