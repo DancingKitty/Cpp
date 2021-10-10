0addf5b5034be5cb24da29d3b5b76cc9c607180e
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2021-10-10 22:58:45 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -10,10 +10,9 @@ int binarysearch(int a[],int n,int k)
 		if(k==a[mid]) return mid;
 		else if(k>a[mid]) start=mid+1;
 		else if(k<a[mid]) end=mid-1;
- 	}
- 	return -1;
+	}
+	return -1;
 }
-
 int main()
 {
 	int n,k;
@@ -21,6 +20,7 @@ int main()
 	int a[n];
 	for(int i=0; i<n; i++)
 	cin>>a[i];
+	sort(a,a+n);
 	cin>>k;
 	int index=binarysearch(a,n,k);
 	cout<<index;
