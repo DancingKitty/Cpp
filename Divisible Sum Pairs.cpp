dc58dba0f309bf12429db35fb7f6ab574f62291e
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2021-10-11 15:31:35 +0530

    Add files via upload

---
+++ Divisible Sum Pairs.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,k,count=0;;
+	cin>>n>>k;
+	int a[n];
+	for(int i=0; i<n; i++)
+	cin>>a[i];
+	sort(a,a+n);
+	for(int i=0; i<n; i++)
+	{
+		for(int j=i; j<n; j++)
+		{
+			if(((a[i]+a[j])%k)==0)
+			{
+				if(j>i)
+				count++;
+			}
+		}
+	}
+	cout<<count;
+}
