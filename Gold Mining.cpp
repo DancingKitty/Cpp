cccfbb902e2acacee1ca204877746450762a675b
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2021-10-13 07:44:18 +0530

    Add files via upload

---
+++ Gold Mining.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	for(int i=0; i<t; i++)
+	{
+	int n,x,y;
+	cin>>n>>x>>y;
+	if((n+1)*(y)>=x)
+	cout<<"YES\n";
+	else 
+	cout<<"NO\n";
+	}
+}
