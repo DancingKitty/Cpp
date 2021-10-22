43de8af55532821839863d8bc3f13a618fb29b30
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2021-10-22 10:46:06 +0530

    Add files via upload

--- Lucky Four.cpp
+++ Lucky Four.cpp
@@ -1,20 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main() 
+int main()
 {
-	int T;
-	cin>>T;
-	while(T--)
+	int t;
+	cin>>t;
+	for(int i=0; i<t; i++)
 	{
-	    int num, c=0,result,ld;
-	    cin>>num;
-	    while(num!=0)
-	    {
-	        ld = num%10;
-	        if(ld==4)
-	        c++;
-	        num=num/10;
-	    }
-	    cout<<c<<endl;
+		int n,temp,count=0;
+		cin>>n;
+		vector<int> v;
+		while(n!=0)
+		{
+			temp=n%10;
+			v.push_back(temp);
+			n=n/10;
+		}
+		int c=v.size();
+		for(int i=0; i<c; i++)
+		{
+			if(v[i]==4)
+			count++;
+		}
+		cout<<count<<"\n";
 	}
 }
