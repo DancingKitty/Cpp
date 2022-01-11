939d74ebf065fb580a80e106eff51a9bf551778b
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-01-11 22:55:33 +0530

    Add files via upload

---
+++ Coldplay.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        int m,s;
+        cin>>m>>s;
+        cout<<floor(m/s)<<"\n";
+    }
+}
