3c131e0f636e12abd743fa17f40d6378ec179dc7
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-01-19 22:19:25 +0530

    Add files via upload

---
+++ EqualCardGame.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        int k;
+        cin>>k;
+        cout<<(52%k)<<"\n";
+    }
+}
