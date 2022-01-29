871f6df13a18303c5552e06920cbaecd1aa9e47e
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-01-30 03:32:26 +0530

    Add files via upload

---
+++ SarthakAndHisMagicGrid.cpp
@@ -0,0 +1,28 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define vi vector<int>
+#define l(n) ll n;cin >> n;
+#define a(n) ll a[n];
+#define b(n) ll b[n];
+#define l2(a,b) ll a,b;cin >> a >> b;
+#define l3(a,b,c) ll a,b,c;cin >> a >> b >> c;
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+int main(){
+    fastio;
+
+    l(t);
+    for( ll i=0; i<t; i++){
+        l(n);
+        for(ll i=0; i<n; i++){
+            for(ll j=0; j<n; j++) cout<<"1 ";
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
