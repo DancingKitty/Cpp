eef9e0dbf5c6b479e7a3268bb25c9df6e1b9feb1
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-01-30 00:39:16 +0530

    Add files via upload

---
+++ ChefAndVacationTransportation.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if((a+b)<c) cout<<"PLANEBUS\n";
+        else if((a+b)>c) cout<<"TRAIN\n";
+        else if((a+b)==c) cout<<"EQUAL\n";
+    }
+}
