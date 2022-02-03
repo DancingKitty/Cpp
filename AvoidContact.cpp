1911c6179959fc805457007ea9be4248e09e0ab4
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-02-03 10:25:09 +0530

    Add files via upload

---
+++ AvoidContact.cpp
@@ -0,0 +1,28 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define vl vector<ll>
+#define lt ll t;cin >> t;
+#define ln ll n;cin >> n;
+#define a(n) ll a[n];
+#define b(n) ll b[n];
+#define lab ll a,b;cin >> a >> b;
+#define labc ll a,b,c;cin >> a >> b >> c;
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+int main(){
+    fastio;
+    
+    lt;
+    for( ll i=0; i<t; i++){
+        ll x,y;
+        cin>>x>>y;
+        if(x>y) cout<<(x+y)<<"\n";
+        else if(x==y) cout<<(x+y)-1<<"\n";
+    }
+
+    return 0;
+}
