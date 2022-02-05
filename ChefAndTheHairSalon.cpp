cb50497d682a8b64e7f8ac4d5a0aae3ed78158a9
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-02-05 10:45:38 +0530

    Add files via upload

---
+++ ChefAndTheHairSalon.cpp
@@ -0,0 +1,20 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+signed main(){
+    fastio;
+    
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){  
+        int n,m;
+        cin>>n>>m;
+        cout<<(n*m)<<"\n";
+    }
+    return 0;
+}
