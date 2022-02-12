0eb5b4a7a791a8eb70fb5b54de6a1d0e59831b16
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-02-13 02:26:27 +0530

    Add files via upload

---
+++ ChefAndNextGen.cpp
@@ -0,0 +1,24 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vsort sort(v.begin(),v.end())
+#define vpb v.push_back
+#define asort sort(a,a+n)
+
+signed main(){
+    fastio;
+    
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){  
+        int a,b,x,y;
+        cin>>a>>b>>x>>y;
+        if((x*y)>=(a*b)) cout<<"Yes\n";
+        else cout<<"No\n";
+    }
+    return 0;
+}
