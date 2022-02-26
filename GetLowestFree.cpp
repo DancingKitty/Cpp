f6fdad6cb4f6b08b673a3a19ca5bf622ecf4fcc8
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-02-26 12:52:45 +0530

    Add files via upload

---
+++ GetLowestFree.cpp
@@ -0,0 +1,24 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vsort sort(v.begin(),v.end())
+#define vpb v.push_back
+#define asort sort(a,a+n)
+
+signed main(){
+    fastio;
+    
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){  
+        int a[3];
+        for(int i=0; i<3; i++) cin>>a[i];
+        sort(a,a+3);
+        cout<<a[1]+a[2]<<"\n";
+    }
+    return 0;
+}
