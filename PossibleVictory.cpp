a78d96d5de7b48652bbf7a4b02819decd2ecf68d
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-02-28 12:30:13 +0530

    Add files via upload

---
+++ PossibleVictory.cpp
@@ -0,0 +1,21 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vsort sort(v.begin(),v.end())
+#define vpb v.push_back
+#define asort sort(a,a+n)
+
+signed main(){
+    fastio;
+ 
+        int r,o,c;
+        cin>>r>>o>>c;
+        if(c+((20-o)*36)>r) cout<<"YES\n";
+        else cout<<"NO\n";
+    
+    return 0;
+}
