a09fd5907c53d5c724950dd937e5db1cc2f63780
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-06-12 01:27:23 +0530

    Add files via upload

---
+++ EqualIntegers.cpp
@@ -0,0 +1,30 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vsort sort(v.begin(),v.end())
+#define vpb v.push_back
+#define asort sort(a,a+n)
+#define vv vector<int> v
+#define rev reverse(a, a+n) 
+
+signed main(){
+    fastio;
+    
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){  
+        int x,y;
+        cin>>x>>y;
+        if(x==y) cout<<0<<"\n";
+        else if(x>y){
+            if(abs(x-y)%2==0) cout<<((x-y)/2)<<"\n";
+            else cout<<((x-y)/2)+2<<"\n";
+        }
+        else cout<<y-x<<"\n";
+    }
+    return 0;
+}
