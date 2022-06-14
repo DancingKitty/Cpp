0a283c56220d05d7b1783f4d3cee1d7d792c35ad
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-06-15 02:53:15 +0530

    Add files via upload

---
+++ GravityFlip.cpp
@@ -0,0 +1,25 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vsort sort(v.begin(),v.end())
+#define vpb v.push_back
+#define asort sort(a,a+n)
+#define vv vector<int> v
+#define rev reverse(a, a+n) 
+
+signed main(){
+    fastio;
+    
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++) cin>>a[i];
+    asort;
+    for(int i=0; i<n; i++) cout<<a[i]<<" ";
+    return 0;
+}
+
