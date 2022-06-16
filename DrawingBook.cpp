4a76608707dbc34e5d71fc25ed6c1bee9f38ebb4
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-06-16 15:51:11 +0530

    Add files via upload

---
+++ DrawingBook.cpp
@@ -0,0 +1,21 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vsort sort(v.begin(),v.end())
+#define vpb v.push_back
+#define asort sort(a,a+n)
+#define vv vector<int> v
+#define rev reverse(a, a+n) 
+
+signed main(){
+    fastio;
+    
+    int n,p;
+    cin>>n>>p;
+    cout<<min(p/2, (n/2-p/2));
+    return 0;
+}
