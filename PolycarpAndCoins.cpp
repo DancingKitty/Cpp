90d87c816656b3d2ffee0572f85906e9c4308b84
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-06-19 10:14:52 +0530

    Add files via upload

---
+++ PolycarpAndCoins.cpp
@@ -0,0 +1,27 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vsort sort(v.begin(),v.end())
+#define vpb v.push_back
+#define asort sort(a,a+n)
+#define vv vector<int> v
+#define rev reverse(a, a+n) 
+
+signed main(){
+    fastio;
+    
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){  
+        int n;
+        cin>>n;
+        if(n%3==0) cout<<n/3<<" "<<n/3<<"\n";
+        else if(n%3==1) cout<<(n/3)+1<<" "<<n/3<<"\n";
+        else if(n%3==2) cout<<n/3<<" "<<(n/3)+1<<"\n";
+    }
+    return 0;
+}
