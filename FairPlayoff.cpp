60e5caa50d94c35e4742211219f05dc43e4da45c
Om Parulekar <65206237+Shiganshina@users.noreply.github.com>
2022-06-22 22:10:26 +0530

    Add files via upload

---
+++ FairPlayoff.cpp
@@ -0,0 +1,30 @@
+//author : elvenblade
+//जय हिंद, जय महाराष्ट्र !
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vsort sort(v.begin(),v.end())
+#define vpb v.push_back
+#define asort sort(a,a+n)
+#define vv vector<int> v
+#define rev reverse(a, a+n) 
+
+signed main(){
+    fastio;
+    
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){  
+        int x,y;
+        int a[4];
+        for(int i=0; i<4; i++) cin>>a[i];
+        x=max(a[0],a[1]);
+        y=max(a[2],a[3]);
+        sort(a,a+4);
+        if((x*y)==(a[2]*a[3])) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+    return 0;
+}
